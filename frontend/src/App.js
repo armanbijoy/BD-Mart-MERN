fb0e7ac54a15e6b7b18d871303a8b154a13d4560
Arman Bijoy <armanbijoy4@gmail.com>
2021-07-10 15:30:46 +0600

    Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
 const App = () => {
   return (
-    <>
-    <Header/>
-    <main className='py-3'>
-      <Container>
-      <HomeScreen/>
-      </Container>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} />
+        </Container>
       </main>
-      <Footer/>
-    </>
-  );
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
