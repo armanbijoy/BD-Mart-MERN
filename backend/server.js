c873e06bc563cc8a19a07ddf3208965583121f37
Arman Bijoy <armanbijoy4@gmail.com>
2021-07-12 01:10:48 +0600

    express, DB added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -15,5 +21,11 @@ app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id)
   res.json(product)
 })
+const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log('Server Running on port 5000'))
+app.listen(
+  PORT,
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on port in ${PORT}`
+  )
+)
